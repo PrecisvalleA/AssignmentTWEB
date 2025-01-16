2ea9cc4a3a2088258ee5f62675c0144db3996607
Alessandro Precisvalle <alessandro.precisvalle@gmail.com>
2025-01-16 15:42:53 +0100

    Connected central server to MongoDB

--- backend/express/server.js
+++ backend/express/server.js
@@ -1,12 +1,21 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 const PORT = 3000;
+const MONGO_URI = 'mongodb://localhost:27017/movieDB'; // Change database name
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the central server!");
+// Connection to MongoDB
+mongoose.connect(MONGO_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Error connecting to MongoDB:', err));
+
+// Endpoint di base
+app.get('/', (req, res) => {
+  res.send('Welcome to the central server!');
 });
 
+// Avvio del server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
