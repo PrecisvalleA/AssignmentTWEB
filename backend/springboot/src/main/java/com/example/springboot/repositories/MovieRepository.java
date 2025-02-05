2b47349bf9b18d82eb91e1f00164c321209fed8a
Alessandro Precisvalle <147641694+PrecisvalleA@users.noreply.github.com>
2025-02-05 12:39:47 +0100

    Delete backend/springboot directory

--- backend/springboot/src/main/java/com/example/springboot/repositories/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.reviewsite.repository;
-
-import com.example.reviewsite.entity.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
