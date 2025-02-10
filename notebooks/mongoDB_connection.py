210dd0895ec437db823afb1c5598de6fef6099f6
Riccardo <riccardocutro@gmail.com>
2025-02-10 12:46:57 +0100

    Completamento della pulizia e del popolamento su MongoDB del file delle recensioni

---
+++ notebooks/mongoDB_connection.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+
+def get_mongo_client():
+    #Connect  to MongoDB and returns the client
+    try:
+        client = MongoClient('localhost', 27017)
+        client.admin.command('ping')  # Test di connessione
+        print("MongoDB connection established!")
+        return client
+    except Exception as e:
+        print(f"Error to connect MongoDB: {e}")
+        return None
+
+def get_mongo_db():
+    #returns the MongoDB database
+    client = get_mongo_client()
+    db = client['reviews']
+    return db
+
