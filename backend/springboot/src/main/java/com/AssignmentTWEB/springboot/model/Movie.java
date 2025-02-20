39b3f52c1d0fcfa82419bae33cd7d38f97541d3a
Riccardo <riccardocutro@gmail.com>
2025-02-20 22:00:03 +0100

    Conclusione dell'entità Movie e Creazione di cartelle utilitarie

---
+++ backend/springboot/src/main/java/com/AssignmentTWEB/springboot/model/Movie.java
@@ -0,0 +1,53 @@
+package com.AssignmentTWEB.springboot.model; // package class
+
+import jakarta.persistence.*; //JPA annotations
+
+@Entity // this class is a JPA entity
+@Table(name = "movies") //table name in the database
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto-increment ID
+    private Long id_movie;
+
+    @Column(nullable = false) //Not Null
+    private String name;
+
+    @Column(columnDefinition = "TEXT") //TEXT type
+    private String description;
+
+    @Column(columnDefinition = "TEXT") //TEXT type
+    private String tagline;
+
+    private double minute;
+
+    private double rating;
+
+    public Movie(){}
+
+    public Movie(Long id_movie, String name, String description, String tagline, double minute, double rating) {
+        this.id_movie = id_movie;
+        this.name = name;
+        this.description = description;
+        this.tagline = tagline;
+        this.minute = minute;
+        this.rating = rating;
+    }
+
+    public Long getId_movie(){return id_movie;}
+    public void setId_movie(Long id_movie){this.id_movie = id_movie;}
+
+    public String getName(){return name;}
+    public void setName(String name){this.name = name;}
+
+    public String getDescription(){return description;}
+    public void setDescription(String description){this.description = description;}
+
+    public String getTagline(){return tagline;}
+    public void setTagline(String tagline){this.tagline = tagline;}
+
+    public double getMinute(){return minute;}
+    public void setMinute(double minute){this.minute = minute;}
+
+    public double getRating(){return rating;}
+    public void setRating(double rating){this.rating = rating;}
+}
